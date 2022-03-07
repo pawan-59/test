99e96818f999860fd476e1135820e14655957e2b
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2022-03-08 00:55:57 +0530

    Update test.py

--- test1/test.py
+++ test1/test.py
@@ -3,16 +3,18 @@ import os
 import requests
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
+hyperlink_format = '<a href="{link}">{text}</a>'
 webhook = DiscordWebhook(url=os.environ["webhook_url"], username="Devtron")
 r = requests.get('https://api.github.com/repos/%s/releases/latest'%os.environ["repo"])
 
 output=json.loads(r.text)
 release_name= output["name"]
 tag_name= output["tag_name"]
+tag_url= output["html_url"]
 body=output["body"]
 
 embed = DiscordEmbed(
-    title="New Release",description="**"+tag_name+"**\n\n"+body, color='03b2f8'
+    title="New Release",description="**"+"[%s](%s)"%(tag_name,tag_url)+"**\n\n"+body, color='03b2f8'
 )
 
 embed.set_author(
