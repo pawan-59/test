9196c487b827a42092ce72c6918dbf8ba56e5de5
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2022-06-28 00:08:14 +0530

    Update script1.js

--- test1/script1.js
+++ test1/script1.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 export let options = {
   vus: 11,
-  duration: '30s',
+  duration: '31s',
 };
 export default function () {
   http.get('http://test.k6.io');
