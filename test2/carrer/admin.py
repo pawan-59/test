f9b336494a6d9738008bc2ff571ba80ebacae2fd
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2022-09-28 23:55:37 +0530

    Delete test2 directory

--- test2/carrer/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.db import models
-from carrer import models
-# Register your models here.
-admin.site.register(models.Jobs)
-admin.site.register(models.Application)
