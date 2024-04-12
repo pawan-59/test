75ff69491a988625cbf8f7168d5363c3850fab78
Pawan Kumar <85476803+pawan-59@users.noreply.github.com>
2024-04-12 15:38:53 +0530

    Create WebDriver.java

---
+++ WebDriver.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+ 
+import io.github.bonigarcia.wdm.WebDriverManager;
+ 
+public class BaseTest {
+     
+    WebDriver driver;
+     
+    @BeforeMethod
+    public void beforeTests() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options=new ChromeOptions();
+        options.setHeadless(true);
+        options.addArguments("window-size=1920,1200");
+        driver=new ChromeDriver(options);
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+ 
+    @AfterMethod
+    public void afterTests() {
+        driver.quit();
+    }
+ 
+}
